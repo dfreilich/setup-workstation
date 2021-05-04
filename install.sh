dfc05c148cb94eb8f69709c772dc0f471dbbef4f
David Freilich <david.freilich@appsflyer.com>
2021-05-04 15:17:52 +0300

    Don't have git branch be like less Ref: https://stackoverflow.com/questions/48341920/git-branch-command-behaves-like-less
    Signed-off-by: David Freilich <david.freilich@appsflyer.com>

--- install.sh
+++ install.sh
@@ -87,6 +87,7 @@ git config --global alias.sta stash
 git config --global alias.llog "log --date=local"
 git config --global alias.flog "log --pretty=fuller --decorate"
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global pager.branch false
 
 # Don't stop if docker fails
 set +e
