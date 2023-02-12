673e0f69591cfb835498856dd69872532acf441b
Dovid Freilich <dfreilich@Dovids-MacBook-Pro.local>
2023-02-12 18:38:43 +0200

    Set git config

--- install.sh
+++ install.sh
@@ -82,6 +82,8 @@ git config --global alias.llog "log --date=local"
 git config --global alias.flog "log --pretty=fuller --decorate"
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
 git config --global pager.branch false
+git config --global user.name "David Freilich"
+git config --global user.email freilich.david@gmail.com
 
 # Don't stop if docker fails
 
